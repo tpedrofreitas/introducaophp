37605193efd7f30e58f34e16e565897ff9261caa
Thiago Freitas <thifreitas26@gmail.com>
2026-03-27 20:54:48 -0300

    Correçâo dos Exercicios

--- basico-pratica/06_desafio.php
+++ basico-pratica/06_desafio.php
@@ -1,5 +1,5 @@
 <?php
-    $salariobruto= "3500.00";
+    $salariobruto= "6000.00";
     $descontototal = 0.0;
 
     $tabela = [
@@ -17,27 +17,14 @@
     <head>
         <meta charset="UTF-08" >
         
-        <title>Idade</title>
+        <title>Calculo INSS</title>
         
     </head>
     <body>
         <?php
-            // if($salariobruto <= 1621.00)
-            // {
-            //     $descontototal = $salariobruto * 0.075;
-            // }elseif($salariobruto <= 2902.84)
-            // {
-            //     $descontototal = ($salariobruto - 1621.00)*0.09 + 121.75;
-            // }elseif ($salariobruto <= 4354.27){
-            //     $descontototal = ($salariobruto - 2902.84)*0.12 + 121.75 + 115.84;
-            // }elseif ($salariobruto <= 8475.55){
-            //     $descontototal = ($salariobruto - 4354.27)*0.14 + 121.75 + 115.84 + 174.17;
-            // }else {
-            //     ($descontototal = 988.08);
-            // };
-            // $salarioliquido = $salariobruto - $descontototal;
+            
             $sobra = $salariobruto;
-
+            /*1ª Faixa
             if($sobra > 0.0){
                 if( $salariobruto > $tabela[0][1] ){
                    $descontototal += $tabela[0][2] * $tabela[0][3] /100.0;
@@ -47,7 +34,7 @@
                     $sobra -= $sobra;
                 }
             }
-
+            //2ª Faixa
             if($sobra > 0.0){
                 if( $salariobruto > $tabela[1][1] ){
                    $descontototal += $tabela[1][2] * $tabela[1][3] /100.0;
@@ -57,7 +44,7 @@
                     $sobra -= $sobra;
                 }
             }
-
+            //3ª Faixa
             if($sobra > 0.0){
                 if( $salariobruto > $tabela[2][1] ){
                    $descontototal += $tabela[2][2] * $tabela[2][3] /100.0;
@@ -67,14 +54,17 @@
                     $sobra -= $sobra;
                 }
             }
-
-            if($sobra > 0.0){
-                if( $salariobruto > $tabela[3][1] ){
-                   $descontototal += $tabela[3][2] * $tabela[3][3] /100.0;
-                   $sobra -= $tabela[3][2];
-                }else{
-                    $descontototal += $sobra * $tabela[3][3] /100.0;
-                    $sobra -= $sobra;
+            4ª Faixa*/
+            for ($faixa = 0; $faixa < count($tabela);$faixa++){
+                       
+                 if($sobra > 0.0){
+                     if( $salariobruto > $tabela[$faixa][1] ){
+                         $descontototal += $tabela[$faixa][2] * $tabela[$faixa][3] /100.0;
+                         $sobra -= $tabela[$faixa][2];
+                     }else{
+                         $descontototal += $sobra * $tabela[$faixa][3] /100.0;
+                         $sobra -= $sobra;
+                     }
                 }
             }
 
